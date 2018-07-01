98f85d33f55732601e6009e8b5e3323126a9f5c4
Karandeep Singh <terminatorkaran@gmail.com>
2018-07-01 12:21:03 -0400

    Added publisher repository

---
+++ src/main/java/com/spring5webApp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.spring5webApp.repositories;
+
+import com.spring5webApp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
